d44fa7eb021b49153de40a2991e8e30bc8b35235
Amar <amark14399@gmail.com>
2023-10-27 10:27:25 +0530

    updated

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 var app = express();
 
 // Setting static and views middleware
-app.use('/static', express.static(path.join(__dirname, 'static')));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 app.set('views',path.join(__dirname, 'views'));
 
 //Use middleware
@@ -12,7 +12,7 @@ app.use(express.urlencoded());
 
 // Endpoints
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'/views/index.html')
+    res.sendFile(path.join(__dirname,'views/signUP.html')
     )});
 
 // Server starts here
